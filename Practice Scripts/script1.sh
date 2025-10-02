116cc26215f3087d3357de402df92bb35c0af9bc
melidevv <mdevarap@charlotte.edu>
2025-10-02 10:30:06 -0400

    Update script1.sh

--- Practice Scripts/script1.sh
+++ Practice Scripts/script1.sh
@@ -1,1 +1,16 @@
-Script 1 content
+#!/bin/bash
+
+add_numbers() {
+    num1=$1
+    num2=$2
+    sum=$((num1 + num2))
+    echo "First number: $num1"
+    echo "Second number: $num2"
+    echo "Sum: $sum"
+    return 0
+}
+
+# Call the function three times with different numbers
+add_numbers 3 9
+add_numbers 15 27
+add_numbers 100 250
